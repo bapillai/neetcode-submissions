171f960a82d24e4e490ae7ef7fc216ad3076cc26
Bharath Uday <10216925+bapillai@users.noreply.github.com>
2026-05-10 21:07:11 +0530

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    preorderTraversal(root) {
+       let result = [];
+       if(root === null) {
+            return result;
+       }
+       let currentNode = root;
+       while(currentNode !== null) {
+            if(currentNode.left === null) {
+                result.push(currentNode.val);
+                currentNode = currentNode.right;
+            } else {
+                let predecessor = currentNode.left;
+                while(predecessor.right !== null && predecessor.right !== currentNode) {
+                    predecessor = predecessor.right;
+                }
+                if(predecessor.right === null) {
+                    result.push(currentNode.val);
+                    predecessor.right = currentNode;
+                    currentNode = currentNode.left;
+                } else {
+                    predecessor.right = null;
+                    currentNode = currentNode.right;
+                }
+            }
+       }
+       return result;
+    }
+}
